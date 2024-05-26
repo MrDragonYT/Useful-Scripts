df6986c433266e08625489b9437853bfea4172e0
hazh10 <hamster2010@outlook.com>
2024-05-26 01:26:19 -0400

    commit

--- shift To Sprint.lua
+++ shift To Sprint.lua
@@ -1,13 +1,19 @@
 -- Courtesy of DragonSpawner12
 
+local uis = game:GetService("UserInputService")
+
 -- Settings (can change)
-local prefix = "/"
+local runSpeed = 25
 -- End of settings
 
-game.Players.PlayerAdded:Connect(function(plr)
-	plr.Chatted:Connect(function(msg)
-		if msg == prefix.."sit" then
-			plr.Character.Humanoid.Sit = true
-		end
-	end)
+uis.InputBegan:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.RightShift then
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = runSpeed
+	end
+end)
+
+uis.InputEnded:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.RightShift then
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+	end
 end)
