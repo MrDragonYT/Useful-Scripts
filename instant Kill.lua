f6debda1ec9d52a0fefea16f014f2c185317ae19
hazh10 <hamster2010@outlook.com>
2024-05-26 02:30:01 -0400

    commit

---
+++ instant Kill.lua
@@ -0,0 +1,10 @@
+-- Courtesy of DragonSpawner12
+
+local part = script.Parent
+
+part.Touched:Connect(function(part)
+    local humanoid = part.Parent:FindFirstAncestorWhichIsA("Model"):FindFirstChild("Humanoid")
+    if humanoid then
+        humanoid:TakeDamage(100)
+    end
+end)
