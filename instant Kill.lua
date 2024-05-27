985843781223018715147a1573ee121416361695
hazh10 <hamster2010@outlook.com>
2024-05-26 20:24:31 -0400

    commit

--- instant Kill.lua
+++ instant Kill.lua
@@ -2,8 +2,8 @@
 
 local part = script.Parent
 
-part.Touched:Connect(function(part)
-    local humanoid = part:FindFirstAncestorWhichIsA("Model"):FindFirstChild("Humanoid")
+part.Touched:Connect(function(hit)
+    local humanoid = hit:FindFirstAncestorWhichIsA("Model"):FindFirstChild("Humanoid")
     if humanoid then
         humanoid:TakeDamage(100)
     end
